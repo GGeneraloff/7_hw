4a35894305ec4a2f828e1161f7fc4e8fd0846234
GGeneraloff <generaloffgeorgy@yandex.ru>
2022-11-28 16:05:21 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -7,12 +7,54 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const film_1 = prompt("Один из последних просмотренных фильмов?", "");
-const rating_1 = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[film_1] = rating_1;
 
-const film_2 = prompt("Один из последних просмотренных фильмов?", "");
-const rating_2 = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[film_2] = rating_2;
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
+for (let i = 0; i < 2; i++) {//первый способ
+    const film = prompt("Один из последних просмотренных фильмов?", "");
+    const rating = prompt("На сколько оцените его?", "");
+    if (film == null||rating==null) {
+        i--;
+    }else if (film == '' || rating==''|| film.length > 50) {
+        i--;
+    } else {
+        personalMovieDB.movies[film] = rating;
+    }
+}
+
+let i = 0;
+while (i < 2) {//второй способ
+    const film = prompt("Один из последних просмотренных фильмов?", "");
+    const rating = prompt("На сколько оцените его?", "");
+    if (film == null || rating == null) {
+        
+    } else if (film == '' || rating == '' || film.length > 50) {
+        
+    } else {
+        personalMovieDB.movies[film] = rating;
+        i++
+    }
+}
+
+do {//третий способ
+    const film = prompt("Один из последних просмотренных фильмов?", "");
+    const rating = prompt("На сколько оцените его?", "");
+    if (film == null || rating == null) {
+
+    } else if (film == '' || rating == '' || film.length > 50) {
+
+    } else {
+        personalMovieDB.movies[film] = rating;
+        i++
+    }
+}
+while (i < 2)
 console.log(personalMovieDB.movies);
